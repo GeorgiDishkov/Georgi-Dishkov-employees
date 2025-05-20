9e093388436908c333ffe9944a823f224dee9228
Georgi <60046070+GeorgiDishkov@users.noreply.github.com>
2025-05-20 19:04:08 +0300

    Init react app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import CsvUploader from "./components/FileReader/FileReader";
+
+function App() {
+  return (
+    <div className="App">
+      <CsvUploader />
+    </div>
+  );
+}
+
+export default App;
